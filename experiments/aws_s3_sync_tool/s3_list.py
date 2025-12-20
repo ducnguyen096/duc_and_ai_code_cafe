cd7aee6b62a207dfa92faaab61cfbd23162d87ef
Duk Nguyen <99184010+ducnguyen096@users.noreply.github.com>
2025-12-20 07:30:32 +0700

    Delete experiments/aws_s3_sync_tool directory

--- experiments/aws_s3_sync_tool/s3_list.py
+++
@@ -1,8 +0,0 @@
-import boto3
-
-s3 = boto3.client('s3')
-
-def list_buckets():
-    """Return a list of bucket names."""
-    response = s3.list_buckets()
-    return [bucket['Name'] for bucket in response['Buckets']]
