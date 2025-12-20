6a06cad2d720de8024a271f5252c53c9dc417875
Duk Nguyen <99184010+ducnguyen096@users.noreply.github.com>
2025-12-20 07:30:59 +0700

    Delete experiments/notion_sync_app_wip directory

--- experiments/notion_sync_app_wip/core/utils.py
+++
@@ -1,9 +0,0 @@
-# core/utils.py
-class Logger:
-    def __init__(self, widget):
-        self.widget = widget
-    def write(self, s):
-        self.widget.insert("end", s)
-        self.widget.see("end")
-        self.widget.update_idletasks()
-    def flush(self): pass
